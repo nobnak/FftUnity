0389ce173d60e75508452f65eccbf2d765fc2962
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-10-23 20:07:40 +0900

    no message

---
+++ Assets/Test/FFT/TestFFT.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFFT : MonoBehaviour {
+
+	void Start () {
+	
+	}
+}
--- Assets/nobnak/FFT/FFT.cs
+++ Assets/nobnak/FFT/FFT.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace nobnak.FFT {
 
-	public class FFT : MonoBehaviour {
+	public class FFT : System.IDisposable {
 		public enum Direction { Forward = 0, Backward }
 
 		public const int NTHREADS_IN_GROUP = 8;
@@ -28,60 +28,72 @@ namespace nobnak.FFT {
 		public const string SHADER_FFT_OUT = "FftOut";
 
 		public ComputeShader fft;
-		public Texture2D photo;
 
-		private int _n;
+		private int _n = -1;
 		private int _nGroups;
 
 		private ComputeBuffer _bitReversalBuf;
-		private uint[] _bitSequences;
-		private uint[] _bitReversals;
 
 		private RenderTexture _fftTexIn;
 		private RenderTexture _fftTexOut;
 
-		void OnDisable() {
-			_fftTexIn.Release();
-			_fftTexOut.Release();
-			_bitReversalBuf.Release();
+		~FFT() { Release(); }
+		#region IDisposable implementation
+		public void Dispose () { Release(); }
+		#endregion
+
+		public Texture Forward(Texture spaceTex) {
+			Init(spaceTex);
+			Graphics.Blit(spaceTex, _fftTexIn);
+			FftX(Direction.Forward);
+			FftY(Direction.Forward);
+			Normalize();
+			return _fftTexIn;
 		}
-		void OnEnable() {
+		public Texture Backward(Texture freqTex) {
+			Init(freqTex);
+			Graphics.Blit(freqTex, _fftTexIn);
+			FftX(Direction.Backward);
+			FftY(Direction.Backward);
+			Normalize();
+			return _fftTexIn;
+		}
+
+		void Release() {
+			if (_fftTexIn != null)
+				_fftTexIn.Release();
+			if (_fftTexOut != null)
+				_fftTexOut.Release();
+			if (_bitReversalBuf != null)
+				_bitReversalBuf.Release();
+		}
+		void Init(Texture texIn) {
+			if (texIn.width != texIn.height) {
+				Debug.Log("Tex width & height must match");
+				return;
+			}
+			if (_n == texIn.width)
+				return;
+
+			Release();
 			_n = photo.width;
 			_nGroups = _n / NTHREADS_IN_GROUP;
 			fft.SetInt(SHADER_N, _n);
 			fft.SetFloat(SHADER_SCALE, 1f / _n);
-
+			
 			_fftTexIn = new RenderTexture(_n, _n, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
 			_fftTexOut = new RenderTexture(_n, _n, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
-			_fftTexIn.filterMode = _fftTexOut.filterMode = photo.filterMode;
+			_fftTexIn.filterMode = _fftTexOut.filterMode = texIn.filterMode;
 			_fftTexIn.enableRandomWrite = _fftTexOut.enableRandomWrite = true;
 			_fftTexIn.Create();
 			_fftTexOut.Create();
 
-			_bitSequences = BitReversal.Sequence(_n);
-			_bitReversals = BitReversal.Reverse(_bitSequences);
-			_bitReversalBuf = new ComputeBuffer(_bitReversals.Length, Marshal.SizeOf(_bitReversals[0]));
-			_bitReversalBuf.SetData(_bitReversals);
+			var bitReversals = BitReversal.Reverse(BitReversal.Sequence(_n));
+			_bitReversalBuf = new ComputeBuffer(bitReversals.Length, Marshal.SizeOf(bitReversals[0]));
+			_bitReversalBuf.SetData(bitReversals);
 		}
-
-		void Start() {
-			Graphics.Blit(photo, _fftTexIn);
-			
-			FftX(Direction.Forward);
-			FftY(Direction.Forward);
-			Normalize();
-			
-			FftX(Direction.Backward);
-			FftY(Direction.Backward);
-			Normalize();
-		}
-		void OnGUI() {
-			var scrRect = new Rect(0.5f * (Screen.width - Screen.height), 0f, Screen.height, Screen.height);
-			Graphics.DrawTexture(scrRect, _fftTexIn);
-		}
-
+		
 		void Swap() { var tmp = _fftTexIn; _fftTexIn = _fftTexOut; _fftTexOut = tmp; }
-
 		void FftX(Direction dir) {
 			var dit = (dir == Direction.Forward ? KERNEL_DIT_X_FORWARD : KERNEL_DIT_X_BACKWARD);
 
@@ -119,7 +131,6 @@ namespace nobnak.FFT {
 				Swap ();
 			}
 		}
-
 		void Normalize() {
 			fft.SetTexture (KERNEL_SCALE, SHADER_FFT_IN, _fftTexIn);
 			fft.SetTexture (KERNEL_SCALE, SHADER_FFT_OUT, _fftTexOut);
